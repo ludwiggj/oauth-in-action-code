42d05dd820e8e9aa4dfcd1c9336cc92de1fabdd0
Graeme Ludwig <graeme.ludwig@itv.com>
2025-08-03 23:08:15 +0100

    Ch 5 ex 1

--- exercises/ch-5-ex-1/authorizationServer.js
+++ exercises/ch-5-ex-1/authorizationServer.js
@@ -30,6 +30,11 @@ var clients = [
   /*
    * Enter client information here
    */
+  {
+	"client_id": "oauth-client-1",
+    "client_secret": "oauth-client-secret-1",
+    "redirect_uris": ["http://localhost:9000/callback"]
+  }
 ];
 
 var codes = {};
@@ -44,28 +49,137 @@ app.get('/', function(req, res) {
 	res.render('index', {clients: clients, authServer: authServer});
 });
 
+// Process the request, validate the client, and send the user to the approval page
 app.get("/authorize", function(req, res){
-	
-	/*
-	 * Process the request, validate the client, and send the user to the approval page
-	 */
-	
+	var client = getClient(req.query.client_id);
+	if (!client) {
+		console.log('Unknown client %s', req.query.client_id);
+		res.render('error', {error: 'Unknown client'});
+		return;
+	} else if (!__.contains(client.redirect_uris, req.query.redirect_uri)) {
+		console.log('Mismatched redirect URI, expected %s got %s', client.redirect_uris, req.query.redirect_uri);
+		res.render('error', {error: 'Invalid redirect URI'});
+		return;
+	} else {
+		var reqid = randomstring.generate(8);
+		requests[reqid] = req.query;
+		res.render('approve', {client: client, reqid: reqid});
+		return;
+	}
 });
 
+// Process the results of the approval page, authorize the client
 app.post('/approve', function(req, res) {
-
-	/*
-	 * Process the results of the approval page, authorize the client
-	 */
+	console.log('Processing approval for request %s', req.body.reqid);
 	
+	var reqid = req.body.reqid;
+	var query = requests[reqid];
+	delete requests[reqid];
+	
+	if (!query) {
+		res.render('error', {error: 'No matching authorization request'})
+		return;
+	}
+
+	if (req.body.approve) {
+		// User approved access
+		if (query.response_type == 'code') {
+			var code = randomstring.generate(8);
+			codes[code] = { request: query };
+			var urlParsed = buildUrl(query.redirect_uri, {
+				code: code,
+				state: query.state
+			});
+			res.redirect(urlParsed);
+			return;
+		} else {
+			var urlParsed = buildUrl(query.redirect_uri, {
+				error: 'unsupported_response_type'
+			});
+			res.redirect(urlParsed);
+			return;
+		}
+	} else {
+		// User denied access
+		var urlParsed = buildUrl(query.redirect_uri, {
+			error: 'access_denied'
+		});
+		res.redirect(urlParsed);
+		return;
+	}
 });
 
+// Process the request, issue an access token
 app.post("/token", function(req, res){
+	var auth = req.headers['authorization'];
+	if (auth) {
+		var clientCredentials = decodeClientCredentials(auth);
+		var clientId = clientCredentials.id;
+		var clientSecret = clientCredentials.secret;
+	}
 
-	/*
-	 * Process the request, issue an access token
-	 */
+	if (req.body.client_id) {
+		if (clientId) {
+			console.log('Client attempted to authenticate with multiple methods');
+			res.status(401).json({error: 'invalid_client'});
+			return;
+		}
+		var clientId = req.body.client_id;
+		var clientSecret = req.body.client_secret;
+	}
 
+	var client = getClient(clientId);
+	if (!client) {
+		console.log('Unknown client %s', clientId);
+		res.status(401).json({error: 'invalid_client'});
+		return;
+	}
+
+	if (client.client_secret != clientSecret) {
+		console.log('Mismatched client secret, expected %s got %s', client.client_secret, clientSecret);
+		res.status(401).json({error: 'invalid_client'});
+		return;
+	}
+
+	if (req.body.grant_type == 'authorization_code') {
+		var code = codes[req.body.code];
+		if (code) { 
+			// Process the valid authorization code in here
+
+			// Code is removed from storage as soon as we know the code is a valid one
+			// regardless of the rest of the processing. This to err on the side of
+			// caution, because a stolen authorization code presented by a bad client
+			// should be considered lost.
+			delete codes[req.body.code]; // burn our code, it's been used
+			if (code.request.client_id == clientId) {
+				// Generate an access token and store it so that we can look it up later.
+				// Access token is opaque in this example, but could be a JWT or other format.
+				var access_token = randomstring.generate();
+				nosql.insert({ access_token: access_token, client_id: clientId }); // not storing scopes in this case (compare to ch 4 ex 4 example)
+
+				console.log('Issuing access token %s', access_token);
+
+				var token_response = { access_token: access_token, token_type: 'Bearer' };
+				res.status(200).json(token_response);
+
+				console.log('Issued tokens for code %s', req.body.code);
+				return;
+			} else {
+				// code was issued to a different client
+				console.log('Client mismatch, expected %s got %s', code.request.client_id, clientId);
+				res.status(400).json({error: 'invalid_grant'});
+				return;
+			}
+		} else {
+			console.log('Unknown code, %s', req.body.code);
+			res.status(400).json({error: 'invalid_grant'});
+			return;
+		}
+	} else {
+		console.log('Unknown grant type %s', req.body.grant_type);
+		res.status(400).json({error: 'unsupported_grant_type'});
+		return;
+	}
 });
 
 var buildUrl = function(base, options, hash) {
